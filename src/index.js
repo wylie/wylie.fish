3dc264ce28410051eb64d1c24fcfa67c2cb5f40c
Wylie Fisher <wylie@dukeofcheese.com>
2018-04-24 14:41:44 -0400

    come on

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { render } from "react-dom";
 import Heading from "./components/heading";
 import List from "./components/list";
 
+import "./styles/_reset.css";
 import "./_index.css";
 
 const App = () => (
