ac6d68ee21749fdb2c51d3c20bd2fbae57e73099
Wylie Fisher <wylie@dukeofcheese.com>
2018-04-25 11:14:53 -0400

    adding in a switch 🎛

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Wrapper from "./components/wrapper";
 
@@ -8,13 +8,14 @@ import "./_index.css";
 
 const App = () => (
   <Router>
-    <React.Fragment>
+    <Switch>
       <Route
-        exact path="/er"
+        exact
+        path="/er"
         component={Wrapper}
       />
       <Redirect from="/" to="/er" />
-    </React.Fragment>
+    </Switch>
   </Router>
 );
 
